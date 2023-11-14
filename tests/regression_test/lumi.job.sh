7762efb453bc98935bd7e115ed0d51b052c89dc3
Diego Ugarte <diego.ugarte@outlook.com>
2023-11-14 18:46:04 +0900

    add minimal version

---
+++ tests/regression_test/lumi.job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --account=XXXXXXXXXX
+#SBATCH --time=02:00:00
+#SBATCH --partition=standard-g
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=8
+
+module load PrgEnv-gnu
+module load craype-x86-trento
+module load craype-accel-amd-gfx90a
+module load rocm
+module load cray-libsci
+
+GENESIS_HOME="../../"
+GENESIS_PATH="${GENESIS_HOME}/src/spdyn/"
+GENESIS_TEST="${GENESIS_HOME}/tests/regression_test/"
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+python3 "${GENESIS_TEST}/test.py" "${GENESIS_PATH}/spdyn" "lumi" "gpu"
